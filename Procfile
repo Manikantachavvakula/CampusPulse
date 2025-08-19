f46e202be35206634478ebef25014a31bf22fd8e
Manikantachavvakula <manikantachavvakula508@gmail.com>
2025-08-19 19:13:34 +0530

    The Final

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn wsgi:app --workers 1 --threads 1 --timeout 120
+web: gunicorn --bind 0.0.0.0:$PORT wsgi:app
--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 from app.app import app
+import os
 
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 8080))  # use Railway's assigned port
     app.run(host="0.0.0.0", port=port)
